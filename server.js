df7770a47bb01be4dcb68f878f2af9958732f086
kachwalUttamsharma <kachwaluttamsharma@gmail.com>
2024-05-16 23:25:46 +0530

    bookmyshowclone

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+require("dotenv").config();
+const app = express();
+const dbConfig = require("./config/dbConfig");
+const userRouter = require("./routes/userRouter");
+const movieRouter = require("./routes/movieRouter");
+const theatreRouter = require("./routes/theatreRouter");
+const bookingRouter = require("./routes/bookingRouter");
+console.log(dbConfig);
+
+app.use(express.static("./public"));
+app.use(express.json());
+app.use("/app/v1/users", userRouter);
+app.use("/app/v1/users/admin", movieRouter);
+app.use("/app/v1/users/theatres", theatreRouter);
+app.use("/app/v1/users/bookings", bookingRouter);
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+app.listen(process.env.PORT, () => {
+  console.log(`server is listening to port no ${process.env.PORT}`);
+});
